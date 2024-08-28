b81264a5adb2e71cc735722b17e1236382deb3b6
schndra <ardnihc@gmail.com>
2024-08-28 15:19:36 +0530

    feat: add types to custom-errors.ts

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -1,41 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from "http-status-codes";
 
 class NotFoundError extends Error {
-  statusCode: any;
-  constructor(message: any) {
+  statusCode: number;
+  constructor(message: string) {
     super(message);
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
 class BadRequest extends Error {
-  statusCode: any;
-  constructor(message: any) {
+  statusCode: number;
+  constructor(message: string) {
     super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
 
 class UnauthenticatedError extends Error {
-  statusCode: any;
-  constructor(message: any) {
+  statusCode: number;
+  constructor(message: string) {
     super(message);
     this.statusCode = StatusCodes.UNAUTHORIZED;
   }
 }
 
 class UnauthorizedError extends Error {
-  statusCode: any;
-  constructor(message: any) {
+  statusCode: number;
+  constructor(message: string) {
     super(message);
     this.statusCode = StatusCodes.FORBIDDEN;
   }
 }
 
-module.exports = {
-  NotFoundError,
-  BadRequest,
-  UnauthenticatedError,
-  UnauthorizedError,
-};
+export { NotFoundError, BadRequest, UnauthenticatedError, UnauthorizedError };
