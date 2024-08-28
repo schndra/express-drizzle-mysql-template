fe749436aecb7aaec70a050fde0be2f13587a743
schndra <ardnihc@gmail.com>
2024-08-28 14:02:35 +0530

    feat: add morgan

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import config from "./config";
 import { userTable } from "./db/schema";
 import { sql } from "drizzle-orm";
 import { StatusCodes } from "http-status-codes";
+import morgan from "morgan";
+
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.get("/", async function (req, res) {
