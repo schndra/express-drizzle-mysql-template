15a9ead8fc697d092b540466ddcbe9047aabd716
schndra <ardnihc@gmail.com>
2024-09-17 19:55:46 +0530

    feat: update schema - change export name userTable to users - remove lastName & age - add password field - add createInsertSchema from @drizzle-zod for validations.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,16 @@
-import { int, mysqlTable, varchar, boolean } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, varchar, text } from "drizzle-orm/mysql-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
-export const userTable = mysqlTable("user", {
+export const users = mysqlTable("user", {
   id: int("id").primaryKey().autoincrement(),
-  name: varchar("name", { length: 255 }),
-  lastName: varchar("last_name", { length: 255 }),
-  age: int("age").notNull(),
+  name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
+  password: text("password").notNull(),
+});
+
+export const insertUserSchema = createInsertSchema(users, {
+  name: z.string().min(3, {
+    message: "name must be at least 3 characters",
+  }),
 });
